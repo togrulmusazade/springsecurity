3e8adf758d0b66e0dec8ecfcfe931c712fb91ef2
togrul <togrulmusazadeonline@gmail.com>
2023-07-24 19:36:48 +0400

    changes

--- src/main/java/com/company/springsecurity/repository/StudentRepository.java
+++ src/main/java/com/company/springsecurity/repository/StudentRepository.java
@@ -1,6 +1,6 @@
-//package com.company.springsecurity.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface StudentRepository extends JpaRepository {
-//}
+package com.company.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository {
+}
